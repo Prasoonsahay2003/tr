638e193541e41dab58207a6f7d814b2ac03f5e76
TusharKumar2003 <tusharkumar.delhi12@gmail.com>
2022-10-29 22:41:12 +0530

    initial commit

--- my-project/src/components/Body.jsx
+++ my-project/src/components/Body.jsx
@@ -3,7 +3,7 @@ export default function Body (){
         <div>
             <div class ="text-5xl">hello world</div>
             <p class = "text-3xl">hello world</p>
-</div>
+        </div>
  
     );
  
